306acf018f3613481da8b47167377a9ee702c05b
rayner <raynerrodriguezv@gmail.com>
2022-03-18 09:38:38 -0400

    completado

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 const ham = document.querySelector(".hamburger");
-
+const close = document.querySelector(".close");
 const sideBar = document.querySelector(".sidebar");
 
 
+// ham.onclick = function() {
+//     sideBar.classList.toggle("active")   /* toggle */ /* hace funcionar el icono de hamb*/ 
+// }
+
+
 ham.onclick = function() {
-    sideBar.classList.toggle("active")
+    sideBar.classList.remove("active")
+}
+
+
+close.onclick = function() {
+    sideBar.classList.add("active")
 }
